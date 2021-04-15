e3c1dbdf0f4c505425450fe20a500d7109d27b67
adrian <adrianhensondev@gmail.com>
2021-04-15 18:24:51 -0400

    Output Julius's review Create review DESTROYING burger joint. Add Reyna's feedback to her review. Create function that displays information about review in a string format.

--- index.js
+++ index.js
@@ -82,6 +82,7 @@ Using the reviews array above:
   1. log only Julius' feedback to the console - no function needed 
 */
 
+console.log(reviews[5].feedback)
 
 
 
@@ -90,7 +91,7 @@ Using the reviews array above do the following: (no function needed)
   1. Following the same format (name, rating, feedback), add a new fictitious review object to the reviews array
   2. log the whole array to the console, make sure the new review is inside of it   
 */
-
+reviews.push({name:"Newman", rating:1, feedback:"I Had High Hopes, But Left Disappointed"})
 
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 5: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 
@@ -99,7 +100,8 @@ Reyna's feedback is missing! Use what you know to do the following: (no function
   2. log the reviews array to the console to check your work
 */
 
-
+reviews[7].feedback="this place is chill with really cool people, great for getting work done on weekdays"
+console.log(reviews)
 
 
 
@@ -114,8 +116,9 @@ Use the getReviewByIndex function below to do the following:
 */
 
 
-function getReviewByIndex(/*Your code here*/) {
-  /*Your code here*/
+function getReviewByIndex(reviewArray, reviewIndex) {
+  const review = reviewArray[reviewIndex];
+  return `${review.name} gave the restaurant a ${review.rating} star review, and their feedback was: ${review.feedback}`
 }
 
 
